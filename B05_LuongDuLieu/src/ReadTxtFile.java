8c45562a891acf146528c14f00c7c0a9357cdfcd
RakiKiet <kiet121004@gmail.com>
2024-03-16 20:34:00 +0700

    laptrinhmang

---
+++ B05_LuongDuLieu/src/ReadTxtFile.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.nio.Buffer;
+
+public class ReadTxtFile {
+    public static void main(String[] args) {
+        try{
+            String src = "D:\\BaiTapDNTU\\LapTrinhMang\\BTJava\\B05_LuongDuLieu\\src\\vidu.txt";
+            File file = new File(src); // Buoc 1
+            // Tạo Inputstream
+            InputStream is = new FileInputStream(file);
+            // Tạo InputStreamReader
+            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+            // Tạo BufferedReader
+            BufferedReader br = new BufferedReader(isr);
+            // Đọc dữ liệu
+            while(true){
+                String line = br.readLine(); // Đọc 1 dòng
+                if (line==null) break; // Nếu không có dữ liệu
+                System.out.println(line); // Xuất ra màn hình
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
